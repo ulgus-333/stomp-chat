6d06975c19a878d0e7bfd4bcfa3b3d8a47d36ebf
jhk <wlgus120332@gmail.com>
2026-01-01 17:25:56 +0900

    init: project init

---
+++ src/main/java/com/practice/stomp/stompchatpractice/StompChatPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.stomp.stompchatpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompChatPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StompChatPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/stomp/stompchatpractice/StompChatPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.stomp.stompchatpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StompChatPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
