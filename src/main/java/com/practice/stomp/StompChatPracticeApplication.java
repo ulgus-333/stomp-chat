e8ea10d0c21ee63600485e190426e7c2fc3d4e6a
ulgus-333 <wlgus120332@gmail.com>
2026-01-02 21:42:17 +0900

    init: SecurityConfig 추가 및 OAuth2 기본 설정 추가 (#1)

--- src/main/java/com/practice/stomp/StompChatPracticeApplication.java
+++ src/main/java/com/practice/stomp/StompChatPracticeApplication.java
@@ -1,4 +1,4 @@
-package com.practice.stomp.stompchatpractice;
+package com.practice.stomp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/practice/stomp/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.practice.stomp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
