8e22c2f294bcbaf0faf27e49a06da0422b75ad2e
jhk <wlgus120332@gmail.com>
2026-01-06 13:27:03 +0900

    feature: 이전 채팅 목록 조회 기능 추가

--- src/main/java/com/practice/stomp/controller/ChatController.java
+++ src/main/java/com/practice/stomp/controller/ChatController.java
@@ -5,18 +5,24 @@ import com.practice.stomp.domain.entity.user.User;
 import com.practice.stomp.domain.request.chat.CreateChatRoomRequestDto;
 import com.practice.stomp.domain.response.chat.ChatRoomResponseDto;
 import com.practice.stomp.domain.response.chat.ChatRoomsResponseDto;
+import com.practice.stomp.domain.response.chat.MessagesResponseDto;
 import com.practice.stomp.service.chat.ChatAggregateService;
 import com.practice.stomp.service.user.UserService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/chats")
 @RestController
 public class ChatController {
@@ -37,5 +43,17 @@ public class ChatController {
         User targetUser = userService.findUserByIdx(requestDto.invitedUserIdx());
         LocalDateTime requestDateTime = LocalDateTime.now();
         return ResponseEntity.ok(chatAggregateService.createChatRoom(requestUser, targetUser, requestDto.title(), requestDateTime));
+    }
+
+    @GetMapping("/{roomIdx}")
+    public ResponseEntity<MessagesResponseDto> getRoomMessages(@AuthenticationPrincipal CustomOAuth2User requestUser,
+                                                               @PathVariable @NotNull Long roomIdx,
+                                                               @PageableDefault(
+                                                                       size = 30,
+                                                                       sort = "created_at",
+                                                                       direction = Sort.Direction.DESC
+                                                               ) Pageable pageable) {
+
+        return ResponseEntity.ok(chatAggregateService.getRoomMessages(requestUser, roomIdx, pageable));
     }
 }
--- src/main/java/com/practice/stomp/repository/MessageRepository.java
+++ src/main/java/com/practice/stomp/repository/MessageRepository.java
@@ -1,7 +1,10 @@
 package com.practice.stomp.repository;
 
 import com.practice.stomp.domain.entity.chat.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    Page<Message> findAllByRoomIdx(Long roomIdx, Pageable pageable);
 }
