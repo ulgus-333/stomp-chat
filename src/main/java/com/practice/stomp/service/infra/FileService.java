39a814e7c116b6aa748299ae8b28dc89a729d10f
jhk <wlgus120332@gmail.com>
2026-01-10 22:36:50 +0900

    refactor: OCI AccessType 분리

--- src/main/java/com/practice/stomp/service/infra/FileService.java
+++ src/main/java/com/practice/stomp/service/infra/FileService.java
@@ -26,7 +26,7 @@ public class FileService {
         CreatePreauthenticatedRequestDetails details = CreatePreauthenticatedRequestDetails.builder()
                 .name("par-" + UUID.randomUUID())
                 .objectName(requestDto.generateFilePath(String.valueOf(requestUser.userIdx())))
-                .accessType(CreatePreauthenticatedRequestDetails.AccessType.ObjectReadWrite)
+                .accessType(CreatePreauthenticatedRequestDetails.AccessType.ObjectWrite)
                 .timeExpires(expiresAt)
                 .build();
 
